6e84261ce2f287309f5b851052ff13347c6e4662
drpramila84 <drpramila84@gmail.com>
2025-12-02 19:57:16 +0530

    Fix LAVALINK_NODES URL syntax error

--- config.js
+++ config.js
@@ -57,7 +57,7 @@ module.exports = {
     DEFAULT_SOURCE: "SC", // YT = Youtube, YTM = Youtube Music, SC = SoundCloud
     // Add any number of lavalink nodes here
     // Refer to https://github.com/freyacodes/Lavalink to host your own lavalink server
-    LAVALINK_NODES: [https://github.com/lavalink-devs/Lavalink.git]
+    LAVALINK_NODES: [https//github.com/lavalink-devs/Lavalink.git]
       {
         host: "lava.godzillaa.com",
         port: 25101,
